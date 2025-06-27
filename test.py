59506f66cdcebef944bb6bd2e4538cb17208f4ba
bobojitney <cchen.andy@gmail.com>
2025-06-27 20:20:26 +0800

    initial commit

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
 print("HELLO WORLD")
 
-print("HOLA")
-print("This is from my-new-branch HELLO")
-
-print("This is from my-new-branch HELLO")
-print("While hola")
-print("Git new 2")
-print("AAAA")
-
